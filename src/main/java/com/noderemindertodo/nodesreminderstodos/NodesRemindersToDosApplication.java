2fa130a2903f3eed64dd87a159f0237dc4f1b31c
Pawel Kwiecinski <pawel.kwiecinski@talan.com>
2023-08-07 12:39:25 +0200

    Initializing spring boot and angular projects + docker-compose

---
+++ src/main/java/com/noderemindertodo/nodesreminderstodos/NodesRemindersToDosApplication.java
@@ -0,0 +1,13 @@
+package com.noderemindertodo.nodesreminderstodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodesRemindersToDosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodesRemindersToDosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noderemindertodo/nodesreminderstodos/NodesRemindersToDosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noderemindertodo.nodesreminderstodos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodesRemindersToDosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
